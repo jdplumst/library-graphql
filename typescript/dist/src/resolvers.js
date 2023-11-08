a408c5e47f9f74aa83c678a4c1aa9eaf7c4b0c6b
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-08 16:46:24 -0500

    Change ID fields to strings

--- typescript/dist/src/resolvers.js
+++ typescript/dist/src/resolvers.js
@@ -1,16 +1,3 @@
 export const resolvers = {
-    Query: {
-        books: async (_, __, context) => {
-            console.log("hi");
-            return await context.db.book.findMany();
-        }
-    },
-    Book: {
-        author: async (parent, args, context) => {
-            console.log(parent);
-            return context.db.author.findFirst({
-                where: { id: Number(parent.author.id) }
-            });
-        }
-    }
+    Query: {}
 };
--- typescript/src/resolvers.ts
+++ typescript/src/resolvers.ts
@@ -1,9 +1,3 @@
-import { ContextValue } from ".";
-
 export const resolvers = {
-  Query: {
-    books: async (_, __, context: ContextValue) => {
-      return await context.db.book.findMany();
-    }
-  }
+  Query: {}
 };
