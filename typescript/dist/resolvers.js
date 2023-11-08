a6477f89177a7443dd4507b61d4fdf1036bf0582
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-11-08 18:05:39 -0500

    Add Book Query

--- typescript/dist/resolvers.js
+++ typescript/dist/resolvers.js
@@ -1,5 +1,12 @@
 export const resolvers = {
     Query: {
+        book: async (_, args, context) => {
+            const book = await context.db.book.findFirst({ where: { id: args.id } });
+            return {
+                ...book,
+                publishedDate: book.publishedDate.toLocaleDateString()
+            };
+        },
         books: async (_, __, context) => {
             const books = await context.db.book.findMany();
             return books.map((b) => ({
--- typescript/src/resolvers.ts
+++ typescript/src/resolvers.ts
@@ -3,6 +3,14 @@ import { QueryBookArgs } from "./generated/resolver-types";
 
 export const resolvers = {
   Query: {
+    book: async (_, args: QueryBookArgs, context: ContextValue) => {
+      const book = await context.db.book.findFirst({ where: { id: args.id } });
+      return {
+        ...book,
+        publishedDate: book.publishedDate.toLocaleDateString()
+      };
+    },
+
     books: async (_, __, context: ContextValue) => {
       const books = await context.db.book.findMany();
       return books.map((b) => ({
